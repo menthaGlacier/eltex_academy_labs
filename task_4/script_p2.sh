8d62d505a3e0f752b6283d105917818a7bc9e3d3
Andrey Litvinenko <dandeliondevelop@gmail.com>
2023-11-08 18:00:07 +0700

    Added p.2. script

---
+++ task_4/script_p2.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Кол-во процессов, которые нужно убить
+kill_count=3
+
+# Получаем PID основного процесса из скрипта п.1.
+parent=$(pgrep -f "script_p1.sh")
+
+# Получаем список дочерних процессов
+pids=($(pgrep -P $parent))
+
+# Получаем кол-во дочерних процессов
+total_pids=${#pids[@]}
+echo $total_pids
+
+# Проверяем, что в списке есть хотя бы необходимое (3) процесса
+if [ $total_pids -ge $kill_count ]; then
+	# Итерируемся по последним элементам списка и беспощадно убиваем процессы
+	for ((i = $total_pids - 1; i >= $total_pids - $kill_count; i--)); do
+		pid_to_kill=${pids[i]}
+		echo "Беспощадно убил процесс с PID $pid_to_kill"
+		kill "$pid_to_kill"
+	done
+else
+	echo "Не хватает детей для убийства"
+fi
